367543f6f00660a135565beae8becd7cd22e8fed
Jessica <jessica.herrera82094@gmail.com>
2018-11-27 18:49:53 -0800

    Complete connecting combine reducer to store

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -6,14 +6,10 @@ const initialState = {
     video: {},
     videos: []
 };
-//var finalState = combineReducers({currentVideoReducer, videoListReducer});
 
-var rootReducer = (combineReducers({video: currentVideoReducer, videoList: videoListReducer}), action) => {
-    
-};
+var rootReducer = combineReducers({currentVideo: currentVideo, videoList: videoList});
 
 //TODO: define the root reducer for this app
-
 //HINT: you'll need to combine the other two reducers in this
 //  app into a single reducer using the 'combineReducers' method
 //  listed above.
--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// const store = createStore(rootReducer,);
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+  );
+
+export default store;
