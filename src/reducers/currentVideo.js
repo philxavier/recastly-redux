52b4d157a877dc518983fe8bfd5a1b64bee22665
Jessica <jessica.herrera82094@gmail.com>
2018-11-29 18:35:50 -0800

    Complete Bare Minimum Requirements

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -4,14 +4,14 @@ import Redux from 'redux';
 //   video: 
 // }
 
-var currentVideoReducer = (state = null, action) => {
+var currentVideoReducer = (state = {}, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
   // console.log('Reducer:', state, action);
   switch(action.type){
     case 'CHANGE_VIDEO':
       var resultObj = {};
       Object.assign(resultObj, state, {video: action.video}); 
-      return resultObj.video || null;
+      return resultObj.video;
     default:
       return state;
   }
